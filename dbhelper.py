8dce036c178521398e373977a680e445200482ec
Jianyang <jyang95@hotmail.com>
2021-07-09 23:49:05 +0800

    Update dbhelper.py

--- dbhelper.py
+++ dbhelper.py
@@ -9,10 +9,9 @@ class DBHelper:
         engine = create_engine(conn_str)
         metadata = MetaData(bind = engine)
         global cheesy_tb
-        cheesy_tb = Table('cheesy_shit', metadata, autoload = True)
+        cheesy_tb = Table('TABLE_NAME', metadata, autoload = True)
 
     def get_items(self):
-        # query = select(cheesy_tb.c.insta_url, cheesy_tb.c.caption).where(cheesy_tb.c.index == index)
         query = select(cheesy_tb.c.insta_url, cheesy_tb.c.caption, cheesy_tb.c.index).order_by(func.random()).limit(1)
         connection = engine.connect()
         result = connection.execute(query)
@@ -22,10 +21,3 @@ class DBHelper:
         query = delete(cheesy_tb).where(cheesy_tb.c.index == index)
         connection = engine.connect()
         ResultProxy = connection.execute(query)
-"""
-    def get_items(self):
-        query = select(cheesy_tb).order_by(func.random())
-        # query = select(cheesy_tb.c.ticker).where(cheesy_tb.c.user_id == user_id)
-        connection = engine.connect()
-        result = connection.execute(query)
-"""
