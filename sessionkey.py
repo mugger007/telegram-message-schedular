2a5a3cb1230c360bed3c997c02c2ad3e47b020b0
Jianyang <jyang95@hotmail.com>
2021-07-09 23:57:09 +0800

    Delete sessionkey.py

--- sessionkey.py
+++
@@ -1,9 +0,0 @@
-from telethon.sync import TelegramClient
-from telethon.sessions import StringSession
-
-api_id = "API_ID"
-api_hash = "API_HASH"
-client = TelegramClient('anon', api_id, api_hash)
-
-with TelegramClient(StringSession(), api_id, api_hash) as client:
-    print(client.session.save())
