49b35b67fd955cdb33d58c2a363de195a0b25395
Jianyang <jyang95@hotmail.com>
2021-07-09 23:47:21 +0800

    Update sessionkey.py

--- sessionkey.py
+++ sessionkey.py
@@ -1,8 +1,8 @@
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
 
-api_id = '5254120'
-api_hash = 'aac1e1210276e4ed0a4e72cd9ffd6bde'
+api_id = "API_ID"
+api_hash = "API_HASH"
 client = TelegramClient('anon', api_id, api_hash)
 
 with TelegramClient(StringSession(), api_id, api_hash) as client:
